a41d61882b7c24fd17230ec6600e892eddab089c
iainyu <iainyu@tencent.com>
2017-09-04 22:19:11 +0800

    Fix a complete bug

--- ftp_v5/multipart_upload.py
+++ ftp_v5/multipart_upload.py
@@ -22,12 +22,13 @@ class MultipartUpload(object):
 
     def __init__(self, cos_client, dict_response):
         self._cos_client = cos_client
-
         for response_name, attr_name, default_value in MultipartUpload.ResponseDataParser:
             if response_name == "Bucket":
                 dict_response[response_name] = dict_response[response_name].split("-")[0]       # V5 返回回来的是bucket-appid这种格式
             setattr(self, attr_name, dict_response[response_name] or default_value)
-            # TODO 要不要持有文件路径
+
+        self._multipart_upload = dict()
+        self._multipart_upload['Part'] = list()
 
     def upload_part(self, content, part_num):
         dict_response = self._cos_client.upload_part(
@@ -36,15 +37,17 @@ class MultipartUpload(object):
             Body=content
         )
 
+        dict_etag = dict()
+        dict_etag['ETag'] = dict_response['ETag']
+        dict_etag['PartNumber'] = part_num
+
+        self._multipart_upload['Part'].append(dict_etag)
+
         return dict_response           # xxx
 
     def complete_upload(self):
-        dict_response = self._cos_client.list_parts(Bucket=self._bucket_name,
-                                                    Key = self._key_name,
-                                                    UploadId=self._upload_id)
-        part_list = dict_response["Part"]
 
         dict_response = self._cos_client.complete_multipart_upload(Bucket=self._bucket_name,
                                                                    Key=self._key_name,
                                                                    UploadId=self._upload_id,
-                                                                   MultipartUpload={"Part":part_list})
+                                                                   MultipartUpload=self._multipart_upload)
