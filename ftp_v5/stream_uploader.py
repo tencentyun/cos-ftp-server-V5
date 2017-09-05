2e37a669a83675bda91ce5db6bc7d53e7b4bf4d4
iainyu <iainyu@tencent.com>
2017-09-05 23:16:48 +0800

    修复一个_min_part_size计算不准确导致无法支持到最大文件的BUG

--- ftp_v5/stream_uploader.py
+++ ftp_v5/stream_uploader.py
@@ -47,7 +47,7 @@ class StreamUploader(object):
         if CosFtpConfig().single_file_max_size > 40 * 1000 * ftp_v5.conf.common_config.GIGABYTE:
             raise ValueError("File size: %d is too big" % CosFtpConfig().single_file_max_size)
 
-        self._min_part_size = int(math.ceil(CosFtpConfig().single_file_max_size / MultipartUpload.MaxiumPartNum / ftp_v5.conf.common_config.MEGABYTE) * ftp_v5.conf.common_config.MEGABYTE);
+        self._min_part_size = int(math.ceil(float(CosFtpConfig().single_file_max_size) / MultipartUpload.MaxiumPartNum));
 
         if CosFtpConfig().single_file_max_size < StreamUploader.MIN_PART_SIZE:
             self._min_part_size = CosFtpConfig().single_file_max_size
