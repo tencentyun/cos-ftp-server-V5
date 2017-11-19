0331f47334e7fb22be4b6c9955b64be59033fc66
yuyang733 <yuyang733>
2017-11-19 22:15:21 +0800

    修复一个多次登录以后，用户名会被删除的bug

--- ftp_v5/cos_ftp_handler.py
+++ ftp_v5/cos_ftp_handler.py
@@ -6,6 +6,3 @@ from pyftpdlib.handlers import FTPHandler
 class CosFtpHandler(FTPHandler):
     def __init__(self, *args, **kwargs):
         FTPHandler.__init__(self, *args, **kwargs)
-
-    def on_logout(self, username):
-        del self.authorizer.user_table[username]
