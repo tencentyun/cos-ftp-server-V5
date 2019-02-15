ae4a40cec6cb598c8273b3e31646922a42b1bde3
Young Yu <yuyang_733@163.com>
2019-02-15 16:27:56 +0800

    设置默认的编码方式为UTF-8

--- ftp_server.py
+++ ftp_server.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
+
 from ftp_v5 import server
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 if __name__ == "__main__":
     server.main()
